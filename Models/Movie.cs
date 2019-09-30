42b587621d801cc06fe2806e8203510902d16058
lindseyshepard <55165590+lindseyshepard@users.noreply.github.com>
2019-09-30 15:56:46 -0400

    Adding folders
    Basic Folders

---
+++ Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Specify Employee Pay Grade")]
+        public float Pay { get; set; }
+        [Required(ErrorMessage = "Labor Hours")]
+        public float Hours { get; set; }
+        [Required(ErrorMessage = "Specify Pay Week")]
+
+        [DataType(DataType.Date)]
+        public DateTime PayWeek { get; set; }
+        //public int ID { get; set; }
+        //public string Title { get; set; }
+
+        //[DataType(DataType.Date)]
+        //public DateTime ReleaseDate { get; set; }
+        //public string Genre { get; set; }
+        //public decimal Price { get; set; }
+    }
+}
